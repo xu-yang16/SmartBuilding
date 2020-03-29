e5d8ac075e29d778880dcc2cade03ab42016dd66
xu-yang16 <2306669517@qq.com>
2020-03-29 19:35:46 +0800

    Temp stable

--- getData.py
+++ getData.py
@@ -24,9 +24,9 @@ if __name__ == '__main__':
                 localtime = time.time()
                 #写入文件
                 with open("state_in_{}_{}.txt".format(room[0],room[1]), "a") as f:
-                    f.write("{:.7f}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(localtime,date,room[0],room[1],t_set,t_now,kaidu_now))
+                    f.write("{:.7f}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(localtime, date, room[0], room[1], t_set, t_now,kaidu_now))
                 #打印输出
-                print(localtime,date,room,t_set,t_now,kaidu_now)
+                print(localtime, date, room, t_set, t_now, kaidu_now)
         except Exception as e:
             print("*********************读取温度数据出现错误...*******************")
         sleep(30)
