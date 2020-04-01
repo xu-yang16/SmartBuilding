8a0271fd4d545bab8d6f8273dc31677b838bd488
xu-yang16 <2306669517@qq.com>
2020-04-01 09:34:23 +0800

    platform is broken

--- PIDControl.py
+++ PIDControl.py
@@ -12,11 +12,11 @@ Kd = 0
 if __name__ == '__main__':
     # 房间号
     # i = [(5, 1, 60), (5, 3, 60), (5, 5, 60), (5, 6, 60), (5, 7, 60), (5, 9, 60), (5, 12, 60)]
-    i = [(5, 1, 100)]
+    i = [(5, 1)]
     error_sum = 0
     # 初始值
     t_set, t_now, kaidu_now = Interface.dataForPID(5, 1)
-    t_set = 18.5
+    t_set = 22.5
     var_open = Kp * (t_now - t_set) + 60
     if var_open >= 100:
         var_open = 100
@@ -32,14 +32,12 @@ if __name__ == '__main__':
                 localtime = time.time()
                 #写入文件
                 with open("state_in_{}_{}.txt".format(room[0],room[1]), "a") as f:
-                    f.write("{:.7f}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(localtime, date, room[0], room[1], t_set, t_now, kaidu_now, var_open))
+                    f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(date, room[0], room[1], t_set, t_now, kaidu_now, var_open))
                 #打印输出
-                print(localtime, date, room, t_set, t_now, kaidu_now)
+                print(localtime, date, room, t_set, t_now, kaidu_now, var_open)
                 
                 # 控制律
-                # error_sum = error_sum + t_now - t_set
-                print("var_open = ", var_open)
-                t_set = 18.5
+                t_set = 22.5
                 var_open = var_open + Kp * (t_now - t_last) + Ki * (t_now - t_set) + Kd * (t_now - t_last)
                 t_last = t_now
                 if var_open >= 100:
@@ -47,10 +45,12 @@ if __name__ == '__main__':
                 elif var_open <= 60:
                     var_open = 60
                 Interface.controlRoom(room[0], room[1], var_open)
+                '''
                 # 写入开度设置
                 with open("setting.txt", "a") as f:
                     f.write("******************Time: {}********************\n".format(time.strftime("%Y-%m-%d %H:%M:%S")))
                     f.write("room: {}, t_set: {}, kaidu: {}\n".format(room[0:2], t_set, var_open))
+                '''
         except Exception as e:
             print("*********************读取温度数据出现错误...*******************")
         sleep(60)
--- getData.py
+++ getData.py
@@ -1,3 +1,6 @@
+'''
+本程序用于读取5层所有房间一天内的温度变化曲线
+'''
 # -*- coding: utf-8 -*-
 # ping 39.100.78.210
 import sys
@@ -8,17 +11,13 @@ from time import sleep
 
 if __name__ == '__main__':
     # 房间号
-    i = [(5, 1, 60), (5, 3, 60), (5, 5, 60), (5, 6, 60), (5, 7, 60), (5, 9, 60), (5, 12, 60)]
-    # i = [(5, 5, 100)]
-    '''
+    i = [(6, 1, 100), (6, 2, 100), (6, 3, 100), (6, 4, 60), (6, 5, 60), (6, 6, 60), (6, 7, 30), (6, 8, 30), (6, 9, 30), (6, 10, 10), (6, 11, 10), (6, 11, 10)]
     for room in i:
         Interface.controlRoom(room[0], room[1], room[2])
     # 写入开度设置
-    with open("setting.txt", "a") as f:
-        f.write("******************Time: {}********************\n".format(time.strftime("%Y-%m-%d %H:%M:%S")))
+    with open("./daily_temp_curve/setting.txt", "a") as f:
         for room in i:
-            f.write("room: {}, kaidu: {}\n".format(room[0:2], room[2]))
-    '''
+            f.write("time:{}\troom:{}\tkaidu:{}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"), room[0:2], room[2]))
     while True:
         try:
             for room in i:
@@ -27,7 +26,7 @@ if __name__ == '__main__':
                 date = time.strftime("%Y-%m-%d %H:%M:%S")
                 localtime = time.time()
                 #写入文件
-                with open("state_in_{}_{}.txt".format(room[0],room[1]), "a") as f:
+                with open("./daily_temp_curve/state_in_{}_{}.txt".format(room[0],room[1]), "a") as f:
                     f.write("{:.7f}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(localtime, date, room[0], room[1], t_set, t_now, kaidu_now))
                 #打印输出
                 print(localtime, date, room, t_set, t_now, kaidu_now)
